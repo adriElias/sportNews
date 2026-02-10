e48ef4a00d1d61785d02284f13d4b23c3805ee4a
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-10 23:17:59 +0100

    fix: rename News class attributes

--- src/main/java/org/sport/BasketballNews.java
+++ src/main/java/org/sport/BasketballNews.java
@@ -4,8 +4,8 @@ public class BasketballNews extends News{
     private String competition;
     private String club;
 
-    public BasketballNews(String headline, String text, double scaring, double price, String competition, String club){
-        super(headline, text, scaring, price);
+    public BasketballNews(String headline, String text, double scoring, double price, String competition, String club){
+        super(headline, text, scoring, price);
         this.competition = competition;
         this.club = club;
     }
