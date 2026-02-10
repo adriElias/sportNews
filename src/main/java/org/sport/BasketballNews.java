b61fd2ab1863ef44360b77bfe4ccb14ceb135fbc
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-10 23:45:35 +0100

    feat: create calculatePriceNews method in BasketballNews class

--- src/main/java/org/sport/BasketballNews.java
+++ src/main/java/org/sport/BasketballNews.java
@@ -1,12 +1,25 @@
 package org.sport;
 
-public class BasketballNews extends News{
+public class BasketballNews extends News {
     private String competition;
     private String club;
 
-    public BasketballNews(String headline, String text, double scoring, double price, String competition, String club){
+    public BasketballNews(String headline, String text, double scoring, double price, String competition, String club) {
         super(headline, text, scoring, price);
         this.competition = competition;
         this.club = club;
+    }
+
+    @Override
+    public double calculatePriceNews() {
+        double price = 250;
+        if (competition.equalsIgnoreCase("Euro League")) {
+            price += 75;
+        }
+        if (club.equalsIgnoreCase("Barça") || club.equalsIgnoreCase("Madrid")) {
+            price += 75;
+        }
+        return price;
+
     }
 }
